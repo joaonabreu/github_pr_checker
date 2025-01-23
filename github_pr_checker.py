2bebb12dae45e378176c07460c502127141aa199
joaonabreu <Joao.Abreu@paddypower.com>
2025-01-23 10:25:21 +0000

    fetch by service

--- github_pr_checker.py
+++ github_pr_checker.py
@@ -1,9 +1,11 @@
 import http.client
 import json
 import os
+import sys
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 env_path = os.path.join(script_dir, '.env')
+
 def load_dotenv():
     with open(env_path) as f:
         for line in f:
@@ -14,14 +16,44 @@ def load_dotenv():
 load_dotenv()
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-REPOS = [
-    "Flutter-Global/fcq-service",
-    "Flutter-Global/psa-service",
-    "Flutter-Global/sco-service",
-    "Flutter-Global/wlp-service",
-    "Flutter-Global/foe-service"
-]
+REPOS = {
+    "fcq": [
+        "Flutter-Global/fcq-service",
+        "Flutter-Global/fcq-chef-ppb",
+        "Flutter-Global/fcq-configrepo-i2-ppb",
+        "Flutter-Global/fcq-configrepo-i2-pp",
+        "Flutter-Global/fcq-configrepo-i2-sbg",    
+    ],
+    "psa": [
+        "Flutter-Global/psa-service",
+        "Flutter-Global/psa-chef-ppb",
+        "Flutter-Global/psa-configrepo-i2-ppb",
+        "Flutter-Global/psa-configrepo-i2-pp",
+        "Flutter-Global/psa-configrepo-i2-sbg",
+    ],
+    "sco": [
+        "Flutter-Global/sco-service",
+        "Flutter-Global/sco-chef-ppb",
+        "Flutter-Global/sco-configrepo-i2-ppb",
+        "Flutter-Global/sco-configrepo-i2-pp",
+        "Flutter-Global/sco-configrepo-i2-sbg",
+    ],
+    "wlp": [
+        "Flutter-Global/wlp-service",
+        "Flutter-Global/wlp-chef-ppb",
+        "Flutter-Global/wlp-configrepo-i2-ppb",
+        "Flutter-Global/wlp-configrepo-i2-pp",
+        "Flutter-Global/wlp-configrepo-i2-sbg",
+    ],
+    "foe": [
+        "Flutter-Global/foe-service",
+        "Flutter-Global/foe-chef-ppb",
+        "Flutter-Global/foe-ansible-ppb",
+    ],
+}
 
+def isPREligible(pr):
+    return "dependabot" not in pr['user']['login']
 
 def print_repo_header(repo):
     print("=" * len(repo))
@@ -29,7 +61,6 @@ def print_repo_header(repo):
     print("=" * len(repo))
 
 def check_open_prs(repo):
-    print_repo_header(repo)
     conn = http.client.HTTPSConnection("api.github.com")
     headers = {
         "Authorization": f"token {GITHUB_TOKEN}",
@@ -41,9 +72,10 @@ def check_open_prs(repo):
     response = conn.getresponse()
     if response.status == 200:
         open_prs = json.loads(response.read().decode())
+        print_repo_header(repo)
         if open_prs:
             for pr in open_prs:
-                if "dependabot" not in pr['user']['login']:
+                if isPREligible(pr):
                     print(f"- #{pr['number']}: {pr['title']} by {pr['user']['login']}")
         else:
             print("No open PRs")
@@ -51,5 +83,9 @@ def check_open_prs(repo):
         print(f"Failed to fetch PRs for {repo}: {response.status}")
     conn.close()
 
-for repo in REPOS:
-    check_open_prs(repo)
+repo = sys.argv[1] if len(sys.argv) > 1 else None
+if repo:
+    for r in REPOS[repo]:
+        check_open_prs(r)
+else:
+    print("You need to specify a repo to check. E.g. python github_pr_checker.py fcq")
